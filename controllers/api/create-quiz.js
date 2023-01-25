5c5233a8e278628f53d74f410c9ae63f20305369
Humzas <humzas@umich.edu>
2023-01-25 17:16:00 -0500

    began work on server routes

---
+++ controllers/api/create-quiz.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const { QuizList } = require("../../models");
+
+router.post("/", async (req, res) => {
+  try {
+    const newQuizList = QuizList.create({
+      ...req.body,
+    });
+    res.status(200).json(newQuizList);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const updateQuizList = QuizList.update(
+      {
+        quiz_title: req.body.title,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    if (!updateQuizList) {
+      res.status(400).json({ message: "No quiz with that ID exists!" });
+      return;
+    }
+    res.status(200).json({ message: "Quiz Name updated successfully!" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
