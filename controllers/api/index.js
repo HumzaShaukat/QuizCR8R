cd78612f4576971791aa02b6783b55faa8d14ef9
hvansalisbury <vansal51@yahoo.com>
2023-01-27 15:43:10 -0500

    set up home routes with profile, login, and signup

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const question = require("./question-routes");
 const quizList = require("./quiz-routes");
+const homeRoutes = require('./home-routes')
 
 router.use("/questions", question);
 router.use("/quizzes", quizList);
+router.use("/", homeRoutes);
 
 module.exports = router;
 //will be filled in after all routes created
