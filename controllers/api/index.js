9c46a4c85040d015e94771ddaae89495a7391940
Jeaustin <JeaustinS27@gmail.com>
2023-01-30 21:45:46 -0500

    fixed routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,7 +4,6 @@ const quizList = require("./quiz-routes");
 const createRoutes = require('./create-routes');
 const userRoutes = require('./user-routes');
 
-router.use("/", homeRoutes);
 router.use("/questions", question);
 router.use("/quizzes", quizList);
 router.use("/create", createRoutes);
