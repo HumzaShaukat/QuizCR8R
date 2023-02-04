f095149b4c128df900c35bd1c402a047406af650
hvansalisbury <vansal51@yahoo.com>
2023-02-03 23:19:39 -0500

    started the take quiz route

---
+++ controllers/api/take-quiz-routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const withAuth = require('../../utils/auth')
+const { User, QuizList, Question } = require('../../models');
+
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const quizData = await QuizList.findByPk(req.params.id, {
+      include: Question,
+    });
+    const quiz = quizData.get({ plain: true });
+    console.log(quiz)
+
+    if (!quizData) {
+      res.status(400).json({ message: "Quiz Not Found" });
+      return;
+    }
+    res.status(200).json({ quiz });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  return;
+});
+
+module.exports = router;
