9a0d6cb5cb52663134be05f24ec67e96c54e8f93
hvansalisbury <vansal51@yahoo.com>
2023-02-04 14:05:39 -0500

    take-quiz should be mmostly complete

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,10 @@
 const sequelize = require('../config/connection');
-const { User, Question, QuizList } = require('../models');
+const { User, Question, QuizList, Score } = require('../models');
 
 const userData = require('./userData.json');
 const quizData = require('./quizData.json');
 const questionData = require('./questionData.json');
+const scoreData = require('./scoreData.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -23,6 +24,12 @@ const seedDatabase = async () => {
   for (const question of questionData) {
     await Question.create({
       ...question,
+    });
+  }
+
+  for (const score of scoreData) {
+    await Score.create({
+      ...score,
     });
   }
 
