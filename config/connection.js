9d652ce7cb4544a16d8994eb66e7885b413e5e71
hvansalisbury <vansal51@yahoo.com>
2023-02-04 20:51:15 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -10,11 +10,11 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
+    // {
+    //   host: 'localhost',
+    //   dialect: 'mysql',
+    //   port: 3306
+    // }
   );
 }
 
